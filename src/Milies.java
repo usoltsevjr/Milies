cd14427687325be8cadeaa3ecb7bac4878548a96
Anton Usoltsev <usoltsevjr@gmail.com>
2020-07-13 10:06:14 +0300

    First commit

---
+++ src/Milies.java
@@ -0,0 +1,7 @@
+public class Milies {
+    public static void main(String[] args) {
+        int ticket = 3600;
+        int bonus = ticket / 20;
+        System.out.println("Miles accrued to you = " + bonus);
+    }
+}
